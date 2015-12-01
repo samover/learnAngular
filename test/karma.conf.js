f4051737299f641e42c9440b081c467e0d1d32f1
samover <samuel.overloop@gmail.com>
2015-12-01 15:33:12 +0000

    karma config

---
+++ test/karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: '../',
+
+    frameWorks: ['jasmine'],
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'js/**/*.js',
+      'test/**/*.spec.js',
+    ],
+
+    exclude: [],
+
+    reporters: ['progress'],
+
+    port: 9876,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: true,
+
+    browsers: ['PhantomJS'],
+
+    singleRun: true
+  });
+};
