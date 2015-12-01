9fb687c7ccb5984c3810c3f38de1ca88a7082179
samover <samuel.overloop@gmail.com>
2015-12-01 21:25:49 +0000

    debugged gitUserSearchController

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -7,13 +7,12 @@ describe('GitUserSearchController', function() {
     ctrl = $controller('GitUserSearchController');
   }));
 
-
-  it('initialises with empty search result and term', function() {
+  it('initialises with an empty search result and term', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
 
-  describe('when searching for user', function() {
+  describe('when searching for a user', function() {
 
     var items = [
       {
@@ -26,20 +25,24 @@ describe('GitUserSearchController', function() {
         "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
         "html_url": "https://github.com/stephenlloyd"
       }
-      ];
+    ];
 
-      var httpBackend;
-      beforeEach(inject(function($httpBackend) {
-        httpBackend = $httpBackend;
-        $httpBackend.when("GET", "https://api.github.com/search/users?q=hello").respond( { items: items });
-      }));
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend;
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+          { items: items }
+        );
+    }));
 
-      it('displays search result', function() {
-        ctrl.searchTerm = 'Hello';
-        ctrl.doSearch();
-        httpBackend.flush();
-        expect(ctrl.searchResult.items).toEqual(items);
-      });
-
+    it('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
     });
   });
+
+});
