390335a90e37964daba22e89a2b0f2c38da49092
samover <samuel.overloop@gmail.com>
2015-12-03 17:00:52 +0000

    no json request with emtpy string

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,11 +1,17 @@
-githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
+githubUserSearch.controller('GitUserSearchController', ['$scope', 'Search', function($scope, Search) {
   var self = this;
 
-  self.doSearch = function() {
-    Search.query(self.searchTerm)
+  $scope.doSearch = function() {
+    Search.query($scope.searchTerm)
     .then(function(response) {
       self.searchResult = response.data;
     });
   };
 
+  $scope.$watch('searchTerm', function(){
+    $scope.searchTerm = ($scope.searchTerm === undefined) ? '' : $scope.searchTerm;
+    if($scope.searchTerm.length > 0) {
+      $scope.doSearch();
+    }
+  });
 }]);
