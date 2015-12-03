9588c5cc2b0c587ba0d1c76b4506755eaceb8f54
samover <samuel.overloop@gmail.com>
2015-12-03 14:46:57 +0000

    add feature test for link to repos and followers

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,6 +1,5 @@
 githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
   var self = this;
-  var q = self.searchTerm;
 
   self.doSearch = function() {
     Search.query(self.searchTerm)
--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,9 @@
 describe('Github Profile Finder', function() {
-  var searchBox = element(by.model('searchCtrl.searchTerm'));
-  var searchButton = element(by.className('btn'));
-  var profiles = element.all(by.repeater('item in searchCtrl.searchResult.items'));
+  var searchBox = element(by.model('searchCtrl.searchTerm')),
+  searchButton = element(by.className('btn')),
+  profiles = element.all(by.repeater('item in searchCtrl.searchResult.items')),
+  repos = element.all(by.className('repos')),
+  followers = element.all(by.className('followers'));
 
   beforeEach(function() {
     browser.get('http://localhost:3000');
@@ -18,11 +20,17 @@ describe('Github Profile Finder', function() {
     });
 
     it('finds profiles', function() {
-      expect(profiles.first().getText()).toEqual('samover');
+      expect(profiles.first().getText()).toContain('samover');
     });
 
-    it('lists shows amount of followers and repos', function() {
+    it('lists link to repos of searched user', function() {
+      var html = '<a href="https://github.com/samover?tab=repositories">repositories</a>';
+      expect(repos.first().getInnerHtml()).toEqual(html);
+    });
 
+    it('lists link to followers of searched user', function() {
+      var html = '<a href="https://github.com/samover/followers"<followers</a>';
+      expect(followers.first().getInnerHtml()).toEqual(html);
     });
   });
 });
