84448cacb3f576ae9276227a91a8286e56cea60d
samover <samuel.overloop@gmail.com>
2015-12-03 16:15:04 +0000

    updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 var githubUserSearch = angular.module('GitUserSearch', ['ngResource']);
 
+githubUserSearch.directive('searchForm', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/partials/search-form.html'
+  };
+});
 
+githubUserSearch.directive('searchResults', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/partials/search-results.html'
+  };
+});
