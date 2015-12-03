dc85ef4137b13ab6b91e463dc8168042ecf21eae
samover <samuel.overloop@gmail.com>
2015-12-03 22:41:12 +0000

    add panels for repo and followers

--- js/profiles.js
+++ js/profiles.js
@@ -4,11 +4,19 @@
   app.controller('ProfileController', ['$http', function($http) {
     var self = this;
 
-    self.profile = {};
+    self.isSet = function(tabNumber) {
+      return self.tab === tabNumber;
+    };
+
+    self.setProfile = function($event) {
+      $event.preventDefault();
+      self.tab = parseInt($event.target.id);
+      self.getProfile($event.target.href);
+    };
 
     self.getProfile = function(url) {
-      $http.get(url).then(function(data) {
-        self.profile = data;
+      $http.get(url).then(function(response) {
+        self.data = response.data;
       });
     };
   }]);
